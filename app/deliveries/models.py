8d3f9178b4d57e2c3c0f8e90045dd9646716311f
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2026-03-17 19:09:01 +0530

    Create models.py

---
+++ app/deliveries/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from apps.webhooks.models import Webhook
+from apps.events.models import Event
+
+class Delivery(models.Model):
+    STATUS_CHOICES = [
+        ("pending", "Pending"),
+        ("success", "Success"),
+        ("failed", "Failed"),
+    ]
+
+    webhook = models.ForeignKey(Webhook, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+    response_code = models.IntegerField(null=True, blank=True)
+    response_body = models.TextField(null=True, blank=True)
+
+    attempt_count = models.IntegerField(default=0)
+    last_attempt_at = models.DateTimeField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.webhook.url} -> {self.status}"
