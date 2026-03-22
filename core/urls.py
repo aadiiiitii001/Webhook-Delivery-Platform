4d44c8b353fca443dfc0976de79c06dc452b50f4
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2026-03-22 17:36:19 +0530

    Create urls.py

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/users/", include("app.users.urls")),
+    path("api/webhooks/", include("app.webhooks.urls")),
+    path("api/events/", include("app.events.urls")),
+    path("api/deliveries/", include("app.deliveries.urls")),
+]
