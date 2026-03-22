3c3bb79ffb0d13df175ebe35c52527312b286558
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2026-03-22 20:30:30 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("app.users.urls")),
-    path("api/webhooks/", include("app.webhooks.urls")),
     path("api/events/", include("app.events.urls")),
     path("api/deliveries/", include("app.deliveries.urls")),
 ]
